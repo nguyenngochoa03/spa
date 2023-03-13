e5eebc73eb361a94f8a7ac23cd7a36e2fabacfa2
haudv3202 <haudang02.it@gmail.com>
2023-03-13 11:48:17 +0700

    hau update

--- app/admin/controllers/QuestionsController.php
+++ app/admin/controllers/QuestionsController.php
@@ -41,17 +41,45 @@ class QuestionsController extends BaseController
                     ]
                 );
                 if ($result){
-                    redirect('success', "Cập nhật thành công!", 'add-question');
+                    redirect('success', "Thêm thành công!", 'add-question');
                 }
             }
         }
     }
 
     public function editQuestion($id){
-        $this->render('admin.UserDisplay.questions.editQuestion');
+        $dataOne = questions::findOne($id);
+        $this->render('admin.UserDisplay.questions.editQuestion',compact('dataOne'));
     }
 
-    public function deleteQuestion($id){
+    public function updateQuestion($id){
+        if(isset($_POST['sb-edit-question'])){
+            $question = $_POST['question'];
+            $reply = $_POST['reply'];
+            $errors = [];
+            if(empty($_POST['question'])){
+                $errors[] = 'Bạn cần nhập câu hỏi';
+            }
+            if(empty($_POST['reply'])){
+                $errors[] = 'Bạn cần nhập tên nội dung trả lời';
+            }
 
+            if(count($errors) > 0){
+                redirect('errors', $errors, 'add-question');
+            }else {
+                $result = questions::updatefind($id,[
+                    "question" => $question,
+                    "reply" => $reply
+                ]);
+                if ($result){
+                    redirect('success', "Cập nhật thành công!", 'add-question');
+                }
+            }
+        }
+    }
+    public function deleteQuestion($id){
+        questions::delete($id);
+        $_SESSION['success'] = "Xóa thành công!";
+        header('location: '.route('questions'));
     }
 }
